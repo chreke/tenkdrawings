9bb02feebfdefc10dc455cfce4680dd39bd72288
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2013-04-20 16:47:00 +0200

    Add login functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
+
+  before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
+
+  private
+
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Tenkdrawings::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
