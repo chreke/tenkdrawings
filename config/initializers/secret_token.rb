f0d3719c4d1878ed9d232bc457f87767c786b893
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2013-04-20 14:57:06 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tenkdrawings::Application.config.secret_token = 'f8d1c8df5428eebd382f97d67ab6e113fd723d0fe346c1274e28aef9259531582a2d0f55c034afff2534c6dcc6a010dc173345447f33fa9f354955c811bb051d'
