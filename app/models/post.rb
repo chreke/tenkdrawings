01b47cd240132794a4e359534e75f910af12b9c8
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2013-04-27 15:11:50 +0200

    Add image attachments to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
+  attr_accessible :image
   belongs_to :user
+  styles = {:medium => "300x300>", :thumb => "100x100>"}
+  has_attached_file :image, styles: styles
 end
