fe97104f376d2614a9884c921b34945626b2518e
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2013-05-01 22:05:12 +0200

    Add methods for calculating level and progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,23 @@ class User < ActiveRecord::Base
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
 
+  def level
+    User.experience_to_level(self.experience).round
+  end
+
+  def progress
+    User.experience_to_level(self.experience) - self.level
+  end
+
   private
+
+  def self.experience_to_level(experience)
+    (experience / 40 + 4.5 ** 2) ** 0.5 - 4.5
+  end
+
+  def self.level_to_experience(level)
+    40 * level ** 2 + 360 * level
+  end
 
   def create_remember_token
     self.remember_token = SecureRandom.urlsafe_base64
