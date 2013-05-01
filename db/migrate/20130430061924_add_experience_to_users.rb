f22f77c3f29761c7a77e3c92398ba100038803a6
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2013-05-01 17:23:59 +0200

    Add experience column to users

---
+++ db/migrate/20130430061924_add_experience_to_users.rb
@@ -0,0 +1,5 @@
+class AddExperienceToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :experience, :integer, default: 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130427121412) do
+ActiveRecord::Schema.define(:version => 20130430061924) do
 
   create_table "posts", :force => true do |t|
     t.integer  "user_id"
@@ -28,10 +28,11 @@ ActiveRecord::Schema.define(:version => 20130427121412) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
     t.string   "password_digest"
     t.string   "remember_token"
+    t.integer  "experience",      :default => 0
   end
 
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
